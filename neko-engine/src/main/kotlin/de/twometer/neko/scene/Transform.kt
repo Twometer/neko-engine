0e81bc168601d5aaa3dd1f32c60be76e9d5cb79b
Twometer <twometer@outlook.de>
2021-03-19 13:03:46 +0100

    Allow for scaling transforms

--- neko-engine/src/main/kotlin/de/twometer/neko/scene/Transform.kt
+++ neko-engine/src/main/kotlin/de/twometer/neko/scene/Transform.kt
@@ -5,17 +5,18 @@ import org.joml.Matrix4f
 import org.joml.Quaternionf
 import org.joml.Vector3f
 
-data class Transform(val translation: Vector3f = Vector3f(), val rotation: Quaternionf = Quaternionf()) {
+data class Transform(val translation: Vector3f = Vector3f(), val rotation: Quaternionf = Quaternionf(), val scale: Vector3f = Vector3f(1f, 1f, 1f)) {
 
     val matrix: Matrix4f
         get() {
-            return Matrix4f().translate(translation).rotate(rotation)
+            return Matrix4f().translate(translation).rotate(rotation).scale(scale)
         }
 
     operator fun times(transform: Transform): Transform {
         return Transform(
             this.translation.clone().add(transform.translation),
-            this.rotation.clone().mul(transform.rotation)
+            this.rotation.clone().mul(transform.rotation),
+            this.scale.clone().mul(transform.scale)
         )
     }
 
