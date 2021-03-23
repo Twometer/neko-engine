2aad3fbdc707ffa6911263948bd07ec9e59d1a6b
Twometer <twometer@outlook.de>
2021-03-23 01:15:37 +0100

    Walking test

--- neko-demo/src/main/kotlin/DemoApp.kt
+++ neko-demo/src/main/kotlin/DemoApp.kt
@@ -4,12 +4,14 @@ import de.twometer.neko.res.AssetManager
 import de.twometer.neko.res.CubemapCache
 import de.twometer.neko.res.ModelLoader
 import de.twometer.neko.scene.Color
+import de.twometer.neko.scene.nodes.ModelNode
 import de.twometer.neko.scene.nodes.PointLight
 import de.twometer.neko.scene.nodes.Sky
 import de.twometer.neko.util.MathF.toRadians
 
 class DemoApp : NekoApp(AppConfig(windowTitle = "Neko Engine Demo")) {
 
+    private lateinit var rin: ModelNode
     private lateinit var sky: Sky
 
     override fun onPreInit() {
@@ -19,11 +21,13 @@ class DemoApp : NekoApp(AppConfig(windowTitle = "Neko Engine Demo")) {
 
     override fun onPostInit() {
         scene.rootNode.attachChild(ModelLoader.load("rin.fbx").also {
-            it.transform.rotation.rotateX(toRadians(-90f)).rotateZ(-1.3f)
-            it.transform.translation.set(0f, 1f, 0f)
+            it.transform.translation.set(0.75f, 0f, 0f)
+            it.transform.scale.set(0.01, 0.01, 0.01)
+            it.playAnimation(it.animations[0])
+            rin = it
         })
 
-        scene.rootNode.attachChild(ModelLoader.load("animegirl.fbx").also {
+        scene.rootNode.attachChild(ModelLoader.load("girl.fbx").also {
             it.transform.translation.set(2f, 0f, 0f)
             it.transform.scale.set(0.01, 0.01, 0.01)
             it.playAnimation(it.animations[0])
@@ -31,7 +35,7 @@ class DemoApp : NekoApp(AppConfig(windowTitle = "Neko Engine Demo")) {
 
         scene.rootNode.attachChild(ModelLoader.load("test.fbx"))
 
-        scene.rootNode.attachChild(ModelLoader.load("Running.fbx").also {
+        scene.rootNode.attachChild(ModelLoader.load("demo-run.fbx").also {
             it.transform.translation.set(5f, 0f, 0f)
             it.transform.rotation.rotateY(toRadians(15f))
             it.transform.scale.set(0.01, 0.01, 0.01)
@@ -50,6 +54,7 @@ class DemoApp : NekoApp(AppConfig(windowTitle = "Neko Engine Demo")) {
 
     override fun onTimerTick() {
         sky.transform.rotation.rotateY(0.0002f)
+        rin.transform.translation.z += 0.025f
     }
 }
 
