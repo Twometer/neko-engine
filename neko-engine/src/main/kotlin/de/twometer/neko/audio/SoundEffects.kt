6ad9fd9b02e5b1f9ef8fdb92d66fbbca690d8314
Twometer <twometer@outlook.de>
2021-03-25 18:45:57 +0100

    Basic layout for sound integration

---
+++ neko-engine/src/main/kotlin/de/twometer/neko/audio/SoundEffects.kt
@@ -0,0 +1,15 @@
+package de.twometer.neko.audio
+
+import org.joml.Vector3f
+
+class SoundEffects {
+
+    fun play(name: String) {
+
+    }
+
+    fun playAt(name: String, position: Vector3f) {
+
+    }
+
+}
---
+++ neko-engine/src/main/kotlin/de/twometer/neko/scene/nodes/SoundSource.kt
@@ -0,0 +1,4 @@
+package de.twometer.neko.scene.nodes
+
+class SoundSource(val gain: Float = 1.0f, val pitch: Float = 1.0f, val rolloff: Float = 1.0f) : Node() {
+}
