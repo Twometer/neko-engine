d3ea7ba4f87b8ed004afea61a2abee5b11d85b2a
Twometer <twometer@outlook.de>
2021-09-30 10:03:20 +0200

    ShaderInject naming convention

--- neko-engine/src/main/kotlin/de/twometer/neko/render/ShaderInject.kt
+++ neko-engine/src/main/kotlin/de/twometer/neko/render/ShaderInject.kt
@@ -7,22 +7,22 @@ import org.joml.Vector2f
 enum class ShaderInject(val inject: (Shader) -> Unit) {
 
     CameraMatrices({
-        it["viewMatrix"] = NekoApp.the!!.scene.camera.viewMatrix
-        it["projectionMatrix"] = NekoApp.the!!.scene.camera.projectionMatrix
+        it["_viewMatrix"] = NekoApp.the!!.scene.camera.viewMatrix
+        it["_projectionMatrix"] = NekoApp.the!!.scene.camera.projectionMatrix
     }),
 
     CameraMatricesInverse({
-        it["viewMatrixInverse"] = NekoApp.the!!.scene.camera.viewMatrix.clone().invert()
-        it["projectionMatrixInverse"] = NekoApp.the!!.scene.camera.projectionMatrix.clone().invert()
+        it["_viewMatrixInverse"] = NekoApp.the!!.scene.camera.viewMatrix.clone().invert()
+        it["_projectionMatrixInverse"] = NekoApp.the!!.scene.camera.projectionMatrix.clone().invert()
     }),
 
     CameraPosition({
-        it["cameraPos"] = NekoApp.the!!.scene.camera.position
+        it["_cameraPosition"] = NekoApp.the!!.scene.camera.position
     }),
 
     ScreenSize({
         val (w, h) = NekoApp.the!!.window.getSize()
-        it["screenSize"] = Vector2f(w.toFloat(), h.toFloat())
+        it["_screenSize"] = Vector2f(w.toFloat(), h.toFloat())
     })
 
 }
