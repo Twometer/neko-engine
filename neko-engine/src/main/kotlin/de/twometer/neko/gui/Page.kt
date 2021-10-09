626fb7e8df3661e4bde15c4969fabfe08558d125
Twometer <twometer@outlook.de>
2021-10-09 12:23:06 +0200

    More and better tools for the GUI Page

--- neko-engine/src/main/kotlin/de/twometer/neko/gui/Page.kt
+++ neko-engine/src/main/kotlin/de/twometer/neko/gui/Page.kt
@@ -1,9 +1,11 @@
 package de.twometer.neko.gui
 
+import com.google.gson.Gson
 import de.twometer.neko.core.NekoApp
 
 open class Page(val path: String) {
 
+    private val gson = Gson()
     private val guiManager = NekoApp.the.guiManager
 
     open fun onDocumentReady() = Unit
@@ -14,11 +16,34 @@ open class Page(val path: String) {
 
     open fun blocksGameInput() = false
 
-    fun close() {
+    open fun close() {
         if (guiManager.page == this)
             guiManager.page = null
     }
 
-    protected fun runScript(script: String) = guiManager.runScript(script)
+    protected fun runScript(script: String): String = guiManager.runScript(script)
+
+    protected fun setProperty(property: String, value: Any) {
+        runScript("$property = ${gson.toJson(value)};")
+    }
+
+    protected fun setElementProperty(elementId: String, property: String, value: String) {
+        runScript("document.getElementById('$elementId')['$property'] = ${escapeJs(value)};")
+    }
+
+    protected fun setElementText(elementId: String, text: String) {
+        runScript("document.getElementById('$elementId').innerText = '${escapeJs(text)}';")
+    }
+
+    protected fun call(function: String, vararg params: Any): String {
+        val serializedParams = params.joinToString(", ") { gson.toJson(it) }
+        return runScript("$function($serializedParams);")
+    }
+
+    protected fun <T> String.parseJson(clazz: Class<T>): T = gson.fromJson(this, clazz)
+
+    private fun escapeJs(str: String): String =
+        str.trimEnd('\\')
+            .replace("'", "\\'")
 
 }
